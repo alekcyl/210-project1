90246b0403566007c921f3694d6833ac351b469d
Drew <89123942+Drewman784@users.noreply.github.com>
2022-10-28 13:30:21 -0400

    Add files via upload

---
+++ Assets/Audio/Credit.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//
+// Credit for the Audio used:
+//
+//
+// Light Fixture sound- https://www.youtube.com/watch?v=X0rhj7J8lkM
+//
+//
+// Industrial Ambience- https://www.youtube.com/watch?v=ATzS13zhcw0
+//
+//
+//
+//
